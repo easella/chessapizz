371f9966b00c9c88909381fcab8f8e58f9efd472
easella <74786054+easella@users.noreply.github.com>
2023-08-24 15:41:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -77,3 +77,5 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
+if __name__ == "__main__":
+    app.run(debug=True)
